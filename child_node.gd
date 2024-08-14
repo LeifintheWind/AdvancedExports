36ee82894dfd3b6192a98544e41a8d06c0f62009
Thomas Mills <tom.mills301@gmail.com>
2024-08-13 21:47:25 -0600

    Update git ignore

--- child_node.gd
+++
@@ -1,4 +0,0 @@
-extends Node2D
-
-func get_resource_info(newResource: NewResource) -> void:
-	print(newResource.teamName)
--- tutorial_resource.gd
+++
@@ -1,6 +0,0 @@
-extends Resource
-class_name SpellResource
-
-@export var name: String
-@export var maximumRange: int = 3
-@export var attack: int = 5
